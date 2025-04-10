999c825b922ee523b10de023e1f0a64841edf7e1
Dobromir NIKOLOV <dobromir.nikolov@al-enterprise.com>
2025-04-10 15:33:08 +0200

    Add basic test app configuration

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { RainbowSDK } from 'rainbow-web-sdk';
+class TestApplication {
+    constructor() {
+        console.log('It works !!');
+    }
+}
+const testApplication = new TestApplication();
