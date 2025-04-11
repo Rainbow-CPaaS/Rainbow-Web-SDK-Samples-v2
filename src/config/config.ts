236fb0c9246633826287f281ec1b73d213544761
Dobromir NIKOLOV <dobromir.nikolov@al-enterprise.com>
2025-04-11 10:24:59 +0200

    config: update config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,15 @@
 //SDK App configuration file to be filled here
+
+
+/**
+ * For the server, you can use :
+ * Sandbox case, the serverHostname is 'sandbox.openrainbow.com'
+ * For production in Rainbow (REALLY IN PRODUCTION), use 'openrainbow.com'
+ * 
+ * The application ID and the secretKey can be obtained from the developpers website https://developers.openrainbow.com/home
+ */
+
+
 export const appConfig = {
     server: 'serverHostname',
     applicationId: 'applicationId',
